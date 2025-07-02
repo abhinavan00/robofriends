a01cbdaec54d80d7af85a6f09cf41b0dff8c5f4d
Abhinav Anand <abhinavanand072@gmail.com>
2025-07-02 17:43:49 +0530

    stepup project with redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import { thunk } from 'redux-thunk';
 import './index.css'; 
 import App from './containers/App';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
 // import registerServiceWorker from './registerServiceWorker';
 
+const logger = createLogger();
+const roorReducer = combineReducers({searchRobots, requestRobots})
+const store = createStore(roorReducer, applyMiddleware(thunk, logger))
 
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store} >
+                <App />
+            </Provider>
+            );
 
 
 // ReactDOM.render(<App />, document.getElementById('root'));
